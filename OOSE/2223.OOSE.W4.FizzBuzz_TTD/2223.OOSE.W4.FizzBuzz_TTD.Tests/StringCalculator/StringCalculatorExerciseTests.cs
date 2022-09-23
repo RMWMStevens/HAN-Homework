277a7e97c8c438d05288a5536d03588c96ce7fba
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2022-09-23 09:18:44 +0200

    StringCalculator #1 & #2

---
+++ OOSE/2223.OOSE.W4.FizzBuzz_TTD/2223.OOSE.W4.FizzBuzz_TTD.Tests/StringCalculator/StringCalculatorExerciseTests.cs
@@ -0,0 +1,39 @@
+﻿using OOSE.W4.FizzBuzz_TTD.StringCalculator;
+using Xunit;
+
+namespace OOSE.W4.FizzBuzz_TTD.Tests.StringCalculator
+{
+    public class StringCalculatorExerciseTests
+    {
+        readonly StringCalculatorExercise sut = new();
+
+        [Fact]
+        public void Add_ShouldReturn0_WhenEmptyString()
+        {
+            Assert.Equal(0, sut.Add(""));
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        public void Add_ShouldReturnSum_ForTwoNumberInputs(string numbers, int expected)
+        {
+            Assert.Equal(expected, sut.Add(numbers));
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        public void Add_ShouldReturnSum_ForThreeNumberInputs(string numbers, int expected)
+        {
+            Assert.Equal(expected, sut.Add(numbers));
+        }
+
+        [Theory]
+        [InlineData("1,2,3,4,5", 15)]
+        [InlineData("1,2,3,4,5,6", 21)]
+        [InlineData("1,2,3,4,5,6,7", 28)]
+        public void Add_ShouldReturnSum_ForVaryingAmountsOfNumberInputs(string numbers, int expected)
+        {
+            Assert.Equal(expected, sut.Add(numbers));
+        }
+    }
+}
---
+++ OOSE/2223.OOSE.W4.FizzBuzz_TTD/2223.OOSE.W4.FizzBuzz_TTD/StringCalculator/StringCalculatorExercise.cs
@@ -0,0 +1,15 @@
+﻿namespace OOSE.W4.FizzBuzz_TTD.StringCalculator
+{
+    public class StringCalculatorExercise
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers)) return 0;
+
+            var parsedNumbers = numbers.Split(',')
+                                       .Select(_ => int.Parse(_));
+
+            return parsedNumbers.Sum();
+        }
+    }
+}
